45ceb225c9c3f886592645e55c3b3ad17091b79f
unknown <aristo@IT-24-L.aristopharma.com>
2021-06-17 15:39:57 +0600

    Folder are added

---
+++ Modern_syntex/index.js
@@ -0,0 +1,175 @@
+// ES6 Features  
+
+// Arrow Function
+// Single line arrow function a return likhte hoy na 
+// doesn't work with new keyword 
+// doesn't care about this , not work wit call and bind
+
+let fun = ()=>10;
+
+let fun2 = ()=>{
+    return 10
+}
+
+//  This keyword confusion clear , 
+
+var javascript ={
+    name : "Js",
+    dev : "google",
+    lib : ["vue","react","angular"],
+    libprint:function () {
+        var self = this;
+        javascript.lib.forEach(function (params) {
+            console.log(`${self.name} loves ${params}`)
+        })
+    }
+}
+
+javascript.libprint();
+
+var javascript2 ={
+    name : "Js",
+    dev : "google",
+    lib : ["vue","react","angular"],
+    libprint:function () {
+        javascript.lib.forEach((params) => {
+            console.log(`${this.name} loves ${params}`)
+        })
+    }
+}
+
+javascript2.libprint();
+
+
+// Truthy or flase value 
+// False, null,0,undefined,"",NaN   These values are false value and all other true
+
+
+
+// Ternary Operator or ? Symbol 
+
+var age = 35 ;
+var type ;
+if (age>18){
+type = "adult";
+}else{
+    type="Child"
+}
+
+// above program solved with ternary operator
+
+var age2 =35;
+var type2 = (age2>35 ) ? "adults":"Child";
+
+// multiple ternary operator 
+
+var age3 = 28;
+var age3 = (age3>20) ? "adult":(age3<10) ? "child":"young";
+
+
+// Array Find Method
+// When return true exit the execution
+// wont't change my main array
+// Has three parameter 1.CurrentValue 2.curentIndex 3.mainArray
+// Also can run without parameter
+
+var num = [1,2,3,4,5,6]
+
+var result = num.find(function (currentValue) {
+    return currentValue >4 ;
+})
+
+console.log(result)
+
+// Multiple parameter
+
+var num2 = [2,3,4,5,6]
+
+var result2 = num2.find(function(currentValue,currentIndex,mainArray) {
+    console.log(currentIndex)
+    console.log(mainArray)
+    return currentValue>4;
+})
+
+console.log(result2)
+
+
+// array findindex method
+// same as find method just return true value index
+
+// Array filter method 
+// this return an array
+// doesn't change main array
+
+// array slice ,splice, concat method
+
+// map Method 
+
+var numbers = [1,2,3,4,5,6]
+var resul = numbers.map((a)=> a*2);
+
+console.log(resul)
+
+
+// Reduce Method important
+
+var red = [1,2,3,4,5,6]
+
+var sum = red.reduce((previousValue,currentValue,curIndex,arr)=>{
+return previousValue+currentValue;
+
+},0)
+
+console.log(sum)
+
+
+// for loop , for in , for of
+
+for (let i=0;i<=5;i++){
+    console.log(i)
+}
+
+// for in object iterator
+
+const myObj = {
+    name:'js',
+    estd:'1995',
+    founder:'Brendan Erich'
+};
+
+for(property in myObj){
+    console.log(property)
+}
+
+// for of for array , for of for index
+var d = [1,2,3]
+for(element of d){
+    console.log(element)
+}
+
+
+// Important Objects methods *** 
+
+const myObj2 = {
+    name:'js',
+    estd:'1995',
+    founder:'Brendan Erich'
+};
+
+var keys = Object.keys(myObj2)
+console.log(keys)
+
+var values = Object.values(myObj2)
+console.log(values)
+
+var entries = Object.entries(myObj2)
+console.log(entries)
+
+// Object short shand (if value and key is same you can write like )
+
+var x =5;
+var myob3 = {
+    name:"Hasan",
+    x
+}
+
---
+++ Pyhton_snippet/Python_snippet.py
@@ -0,0 +1,68 @@
+#Python_365
+#Day_1
+#Python_snippets
+#pythonprogramming 
+#python3
+#pythoncoding 
+#pythondeveloper
+
+
+
+# # To check your Python verson inside a script:
+
+# import sys
+# print(sys.version)
+
+
+# Output : 3.7.9
+
+# # # To check where is Python installed inside a script:
+
+# import sys,os
+# print(os.path.dirname(sys.executable))
+
+# Output : C:\Users\aristo\AppData\Local\Programs\Python\Python37
+
+
+#         # PIP --> Python package management system (Part-1) 
+
+#     1. Install Package 
+
+#             --👀> pip install <package_name>
+
+#     2. Update package 
+
+#             --👀> pip install <package_name> --upgrade
+#                                 or
+#             --👀> pip install -U <package_name>
+
+
+#     3. Uninstall package 
+
+#             --👀> pip uninstall <package_name> 
+
+
+# #                    #    PyGems.com
+
+
+     # PIP(LIST) --> Python package management system (Part-2) 
+
+         1. See Installed Packages  
+
+                --👀> pip list
+
+        2. See Local Installed Packages 
+
+                --👀> pip list -l
+
+        3. Outdated Packages 
+
+                --👀> pip list -o 
+    
+        4. Up-to-date packages
+
+                --👀> pip list -u 
+
+
+
+#                    #    PyGems.com
