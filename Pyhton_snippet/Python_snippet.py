f6e995fb55e9fe9e328f16acbb0be734a64a93fa
unknown <aristo@IT-24-L.aristopharma.com>
2021-07-11 12:25:33 +0600

    Day 17 added python string method

--- Pyhton_snippet/Python_snippet.py
+++ Pyhton_snippet/Python_snippet.py
@@ -318,17 +318,166 @@
 # These
 # """
 
-#Day-15
-#Python Data Types (string) - Part 1
+# #Day-16
+# #Python Data Types (string) - Part 1
 
-A string is a sequence of characters enclosed in single ('') or double (" ") quotation marks.
-Note that string is ordered and immutable which means you can access it's elements by index but each time one makes any changes
-to the string, completely new string object will be created.
+# A string is a sequence of characters enclosed in single ('') or double (" ") quotation marks.
+# Note that string is ordered and immutable which means you can access it's elements by index but each time one makes any changes
+# to the string, completely new string object will be created.
 
-Example :
+# Example :
 
-a_str = 'Hello World'
-b_str = "Hello World"
+# a_str = 'Hello World'
+# b_str = "Hello World"
+
+# # #                   #    PyGems.com
+#                #https://www.facebook.com/pygems
+
+
+
+# capitalize(),title(),upper(),lower(),casefold()
+
+# #Day-16
+# #Python3_Basics
+# #Python Data Types (string) - Part 2
+# #String methods - capitalize()
+
+
+
+# capitalize(): Converts first character of a string to uppercase letter and lowercases all other characters and doesn't take any parameter.
+
+# Example:
+#           string = "i love Python."
+#           print(string.capitalize())
+
+#           Output : "I love python."
+
+
+#                              PyGems.com
+# #                #https://www.facebook.com/pygems
+
+# title():
+
+# Example:
+# string = "i love Python."
+# print(string.capitalize())
+
+# Output : "I love python."
 
 # #                   #    PyGems.com
 #                #https://www.facebook.com/pygems
+
+
+###
+#  capitalize() - Converts the first character to upper case
+
+
+
+#  center() - Returns a centered string
+#  count() -  Returns the number of times a specified value occurs in a string
+#  encode() - Returns an encoded version of the string
+#  endswith() - Returns true if the string ends with the specified value
+#  expandtabs() - Sets the tab size of the string
+#  find() - Searches the string for a specified value and returns the position of where it was found
+#  format() - Formats specified values in a string
+#  format_map() - Formats specified values in a string
+#  index() -  Searches the string for a specified value and returns the position of where it was found
+#  isalnum() -  Returns True if all characters in the string are alphanumeric
+#  isalpha() -  Returns True if all characters in the string are in the alphabet
+#  isdecimal() -  Returns True if all characters in the string are decimals
+#  isdigit() -  Returns True if all characters in the string are digits
+#  isidentifier() - Returns True if the string is an identifier
+#  islower() -  Returns True if all characters in the string are lower case
+#  isnumeric() -  Returns True if all characters in the string are numeric
+#  isprintable() -  Returns True if all characters in the string are printable
+#  isspace() -  Returns True if all characters in the string are whitespaces
+#  istitle() -  Returns True if the string follows the rules of a title
+#  isupper() -  Returns True if all characters in the string are upper case
+#  join() - Joins the elements of an iterable to the end of the string
+#  ljust() -  Returns a left justified version of the string
+#  lstrip() - Returns a left trim version of the string
+#  maketrans() -  Returns a translation table to be used in translations
+#  partition() -  Returns a tuple where the string is parted into three parts
+#  replace() -  Returns a string where a specified value is replaced with a specified value
+#  rfind() -  Searches the string for a specified value and returns the last position of where it was found
+#  rindex() - Searches the string for a specified value and returns the last position of where it was found
+#  rjust() -  Returns a right justified version of the string
+#  rpartition() - Returns a tuple where the string is parted into three parts
+#  rsplit() - Splits the string at the specified separator, and returns a list
+#  rstrip() - Returns a right trim version of the string
+#  split() -  Splits the string at the specified separator, and returns a list
+#  splitlines() - Splits the string at line breaks and returns a list
+#  startswith() - Returns true if the string starts with the specified value
+#  strip() -  Returns a trimmed version of the string
+#  translate() -  Returns a translated string
+#  upper() -  Converts a string into upper case
+#  zfill() -  Fills the string with a specified number of 0 values at the beginning
+###
+
+
+# Day-17
+# Python3_Basics
+# Python Data Types (string) - Part 3
+# String methods - capitalize()
+
+#  title() -  Converts the first character of each word to upper case
+
+# text1 = 'I am a python Developer.'
+# text2 = '140202223 bu15 bu*f 25'
+# text3 = "He's an python Developer, isn't he?"
+
+
+# print(text1.title())
+# print(text2.title())
+# print(text3.title())
+
+# # Output:
+
+# I Am A Python Developer.
+# 140202223 Bu15 Bu*F 25
+# He'S An Python Developer, Isn'T He?
+
+
+# # lower() -  Converts a string into lower case
+
+
+# print(text1.lower())
+# print(text2.lower())
+# print(text3.lower())
+
+# i am a python developer.
+# 140202223 bu15 bu*f 25
+# he's an python developer, isn't he?
+
+# #casefold() - Converts string into lower case (aggressive lower())
+
+# ***For example, the German lowercase letter ß is equivalent to ss. However, since ß is already lowercase, the lower() method does nothing to it.
+# But, casefold() converts it to ss**
+
+# #  swapcase() - Swaps cases, lower case becomes upper case and vice versa
+
+# print(text1.swapcase())
+# print(text2.swapcase())
+# print(text3.swapcase())
+
+# Output:
+
+# i AM A PYTHON dEVELOPER.
+# 140202223 BU15 BU*F 25
+# hE'S AN PYTHON dEVELOPER, ISN'T HE?
+
+
+# #                   #    PyGems.com
+#                #https://www.facebook.com/pygems
+
+
+
+# Day-18
+# Python3_Basics
+# Python Data Types (string) - Part 4
+# String methods - capitalize()
+
+
+
+#                           #    PyGems.com
+#                    #https://www.facebook.com/pygems
