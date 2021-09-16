8631cf4168643cef4e13ab6672f9552b39030b12
Mamun <a.a.mamunbu@gmail.com>
2021-09-16 09:22:14 +0600

    test file added

--- Pyhton_snippet/test.py
+++ Pyhton_snippet/test.py
@@ -1,8 +1,45 @@
-def reverse_recursion(s):
-    if len(s) == 0:
-        return s
-    else:
-        return reverse_recursion(s[1:]) + s[0]
+# from collections import defaultdict
+# d = defaultdict(list)
+# list1=[]
+
+# n, m = map(int,input().split())
+
+# for i in range(0,n):
+#     d[input()].append(i+1) 
 
 
-print(reverse_recursion("12345"))
+from fractions import Fraction  
+def answer(pegs):
+    arrLength = len(pegs)
+    if ((not pegs) or arrLength == 1):
+        return [-1,-1]
+
+    even = True if (arrLength % 2 == 0) else False
+    sum = (- pegs[0] + pegs[arrLength - 1]) if even else (- pegs[0] - pegs[arrLength -1])
+
+    if (arrLength > 2):
+        for index in range(1, arrLength-1):
+            sum += 2 * (-1)**(index+1) * pegs[index]
+
+    FirstGearRadius = Fraction(2 * (float(sum)/3 if even else sum)).limit_denominator()
+
+    # now that we have the radius of the first gear, we should again check the input array of pegs to verify that
+    # the pegs radius' is atleast 1.
+    # since for valid results, LastGearRadius >= 1 and FirstGearRadius = 2 * LastGearRadius
+    # thus for valid results FirstGearRadius >= 2
+
+    if FirstGearRadius < 2:
+        return [-1,-1]
+
+    currentRadius = FirstGearRadius
+    for index in range(0, arrLength-2):
+        CenterDistance = pegs[index+1] - pegs[index]
+        NextRadius = CenterDistance - currentRadius
+        if (currentRadius < 1 or NextRadius < 1):
+            return [-1,-1]
+        else:
+            currentRadius = NextRadius
+
+    return [FirstGearRadius.numerator, FirstGearRadius.denominator]
+
+print(answer([4,30,50]))
