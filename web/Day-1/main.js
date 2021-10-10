28a6066f2293347a629720591fd8be651cd32265
Mamun <a.a.mamunbu@gmail.com>
2021-10-10 15:59:40 +0600

    Day-1 updated

---
+++ web/Day-1/main.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel')
+console.log(panels)
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click',()=>{
+        removeActiveClasees()
+        panel.classList.add('active')
+
+    })
+})
+
+function removeActiveClasees(){
+    panels.forEach((panel)=>{
+        panel.classList.remove('active')
+    })
+}
